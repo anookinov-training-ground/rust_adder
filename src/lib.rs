3a749e520dc7e1ec947df4645def67dea5ffe44d
Alexander Nookinov <anookinov@gmail.com>
2021-08-22 02:16:48 +0700

    add assert_eq and assert_ne

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,42 @@
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+        // assert_eq!(add_two(2), 4);
+        assert_ne!(4, 3);
+    }
+    
     #[test]
     // fn it_works() {
     //     assert_eq!(2 + 2, 4);
@@ -12,4 +49,22 @@ mod tests {
     fn another() {
         panic!("Make this test fail");
     }
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+        // self.width < other.width && self.height > other.height
+    }
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+    // a + 3
 }
