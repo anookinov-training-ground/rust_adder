7e30b700e1ceb48e15e2cce9a04ea4b01a6df4fd
Alexander Nookinov <anookinov@gmail.com>
2021-08-22 01:54:45 +0700

    change test function name and add failed test

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
+    // fn it_works() {
+    //     assert_eq!(2 + 2, 4);
+    // }
+    fn exploration() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn another() {
+        panic!("Make this test fail");
     }
 }
