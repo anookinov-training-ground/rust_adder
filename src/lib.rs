d960cff4c1930183dac0912028eda24034e3e037
Alexander Nookinov <anookinov@gmail.com>
2021-08-22 11:03:33 +0700

    add more ways to deal with test

--- src/lib.rs
+++ src/lib.rs
@@ -36,7 +36,32 @@ mod tests {
         // assert_eq!(add_two(2), 4);
         assert_ne!(4, 3);
     }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
     
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+
     #[test]
     // fn it_works() {
     //     assert_eq!(2 + 2, 4);
@@ -67,4 +92,33 @@ impl Rectangle {
 pub fn add_two(a: i32) -> i32 {
     a + 2
     // a + 3
+}
+
+pub fn greeting(name: &str) -> String {
+    // format!("Hello {}!", name)
+    String::from("Hello!")
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                // "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                // "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        }
+
+        Guess { value }
+    }
 }
