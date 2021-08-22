738d751e585a6e426da6debd179e87086b8c0f9d
Alexander Nookinov <anookinov@gmail.com>
2021-08-22 11:19:53 +0700

    add controlling how tests are run examples

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+// `cargo test --help` to display the options used with cargo test
+// `cargo test -- -- help` to display the options used after the separator -- (test libary)
+// i.e. cargo test -- --test-threads=1
+// i.e. cargo test -- --show-output
+// i.e. cargo test one_hundred
+// i.e. cargo test add
+// i.e. cargo test -- --ignored
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -63,6 +70,39 @@ mod tests {
     }
 
     #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
+    }
+
+    #[test]
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
+    }
+
+    #[test]
+    #[ignore]
+    fn expensive_test() {
+        // code that takes an hour to run
+    }
+
+    #[test]
     // fn it_works() {
     //     assert_eq!(2 + 2, 4);
     // }
@@ -121,4 +161,9 @@ impl Guess {
 
         Guess { value }
     }
+}
+
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    10
 }
